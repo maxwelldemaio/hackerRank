293e3c24234c742c38b241c03aecca85d71947ea
Maxwell DeMaio <maxdman321@gmail.com>
2021-02-07 09:47:48 -0500

    Completed longest common prefix

---
+++ leetcode_js/LongestCommonPrefix2.js
@@ -0,0 +1,34 @@
+var longestCommonPrefix = function(strs) {
+    if (strs.length == 0) {
+        return '';
+    }
+    if (strs.length == 1) {
+        return strs[0];
+    }
+
+    let longest = '';
+    let comparWord = strs[0];
+    let comparIndex = 0;
+
+    // Loop over each letter in strs[0]
+    for (let comparLetter of comparWord) {
+        // Loop over every word from strs[1:]
+        for (let i = 1; i < strs.length; i++) {
+            // Check for match
+            // Also check if we're going out of bounds
+            currLetter = strs[i][comparIndex];
+            if (comparIndex > strs[i].length || 
+                    comparLetter != currLetter) {
+                return longest;
+            }
+        }
+        // Check next set of chars at strs[1:]
+        // Add to longest commmon prefix
+        comparIndex++;
+        longest += comparLetter
+    }
+    return longest;
+}
+
+// Tests
+console.log(longestCommonPrefix(['flower', 'fly', 'flight']));
