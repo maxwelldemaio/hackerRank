a64e9c4288a0bff96a6d53ca749c5cda729e2837
Maxwell DeMaio <maxdman321@gmail.com>
2021-02-09 08:08:18 -0500

    Completed PowerOfThree

---
+++ leetcode_js/PowerOfThree.js
@@ -0,0 +1,23 @@
+function log3(val) {
+    return Math.log(val) / Math.log(3);
+}
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if (n < 1) {
+            return false;
+    }
+
+    while (n % 3 == 0) {
+        n /= 3;
+    }
+
+    return n == 1;  
+};
+
+console.log(isPowerOfThree(9));
+console.log(isPowerOfThree(27));
+console.log(isPowerOfThree(45));
