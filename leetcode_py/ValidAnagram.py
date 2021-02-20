c8ee24a8733a61e6c22ceb532251758855abf363
Maxwell DeMaio <maxdman321@gmail.com>
2021-02-19 21:43:02 -0500

    Finished valid anagram and started valid palindrome

--- leetcode_py/ValidAnagram.py
+++ leetcode_py/ValidAnagram.py
@@ -1,7 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sHash = {}
+        if len(s) != len(t):
+            return False
 
+        sHash = {}
         # Create map from first string
         for char in s:
             if char in sHash:
@@ -13,7 +15,12 @@ class Solution:
         for char in t:
             if char in sHash:
                 if sHash[char] < 1:
-                    return false
+                    return False
                 sHash[char] -= 1
             else:
-                return false
+                return False
+        return True
+
+# Tests
+example = Solution()
+print(example.isAnagram(s="anagram", t="nagaram"))
