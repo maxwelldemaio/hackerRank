ba97e229ca6edb1eabfac11c2b335502a7fb5d3d
Maxwell DeMaio <maxdman321@gmail.com>
2021-05-20 23:03:03 -0400

    Finished makeTags

--- codingbat_java/src/com/codingbat/Problem.java
+++ codingbat_java/src/com/codingbat/Problem.java
@@ -133,5 +133,17 @@ public class Problem {
 		 * */
 		return a + b + b + a;
 	}
+	
+	
+	public String makeTags(String tag, String word) {
+		 /* 
+		  * The web is built with HTML strings like "<i>Yay</i>" 
+		  * which draws Yay as italic text. In this example, 
+		  * the "i" tag makes <i> and </i> which surround the word "Yay". 
+		  * Given tag and word strings, create the HTML string with 
+		  * tags around the word, e.g. "<i>Yay</i>".
+		  * */ 
+		return "<" + tag + ">"+ word + "</" + tag + ">";
+	}
 
 }
